f900cc3fad913f21d39a5af2a4fb1b05937d1a5f
vtonsor <30914395+vtonsor@users.noreply.github.com>
2021-08-31 17:30:01 -0500

    commit

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -3,51 +3,48 @@ package org.launchcode.hellospring.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * Created by Chris Bay
- */
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
-    //    // Handle request at path /hello
+    //handles requests @ /hello
 //    @GetMapping("hello")
 //    @ResponseBody
-//    public String hello() {
+//    public String hello(){
 //        return "Hello, Spring!";
 //    }
-
+    //lives at /hello/goodbye
     @GetMapping("goodbye")
-    @ResponseBody
-    public String goodbye() {
+    public String goodbye(){
         return "Goodbye, Spring!";
     }
 
-    // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
+    //handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello") //handles post & get methods
+    public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    // Handles requests of the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
+    //handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
 
-
+    //lives @ /hello/form
     @GetMapping("form")
-    @ResponseBody
-    public String helloForm() {
+    public String helloForm(){
         return "<html>" +
                 "<body>" +
-                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
-                "<input type = 'submit' value = 'Greet Me!' >" +
+                "<form method = 'post' action='hello' >" + //submit a request to /hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value= 'Greet Me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+
+
     }
 
 }
